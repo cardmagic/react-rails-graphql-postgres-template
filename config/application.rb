fedb4768d7ea2a8a24c32983df3c2f2966b49c93
Lucas Carlson <lucas@carlson.net>
2018-08-25 22:54:42 -0700

    Primary ids should be UUIDs

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module MyAppApiGateway
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
